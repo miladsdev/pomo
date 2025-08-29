3e262e415eb54c555fc8e7367c3b21645618f350
milad <miladsoltanidev@gmail.com>
2025-08-30 00:58:50 +0200

    design

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,52 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="bg-gray-200 p-3 h-screen">
+    <div class="container container-sm mx-auto">
+      
+      
+      <div class="navbar bg-base-100 shadow-sm mb-3">
+        <div class="navbar-start">
+          <a class="btn btn-ghost text-xl">POMO</a>
+        </div>
+        <div class="navbar-end">
+            <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+        <div class="card bg-base-100 w-full shadow-sm">
+        
+          <div class="bg-red-500 rounded-t-lg flex justify-center">
+            <div class="grid auto-cols-max grid-flow-col gap-5 text-center">
+              <div class="rounded-box text-white flex flex-col py-8 px-9 text-2xl">
+                <span class="countdown font-mono text-9xl font-black">
+                  <span style="--value:24;" aria-live="polite" aria-label="24">24</span>
+                </span>
+                min
+              </div>
+              <div class="rounded-box text-white flex flex-col py-8 px-9 text-2xl">
+                <span class="countdown font-mono text-9xl font-black">
+                  <span style="--value:59;" aria-live="polite" aria-label="59">59</span>
+                </span>
+                sec
+              </div>
+            </div>
+          </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <div class="card-body">
+          <h2 class="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        </div>
+      </div>
     </div>
-  </header>
+  </div>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container {
+  max-width: 480px;
 }
 </style>
