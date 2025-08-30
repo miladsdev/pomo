587dc551e1e38355a6ac45e9b12b86737ebfc319
milad <miladsoltanidev@gmail.com>
2025-08-30 19:27:00 +0200

    css changes

--- src/components/MinSec.vue
+++ src/components/MinSec.vue
@@ -10,13 +10,13 @@ const { round, minutes, seconds } = storeToRefs(pomodoro);
 <template>
     <div class="rounded-t-lg flex justify-center bg-gray-300">
         <div class="grid auto-cols-max grid-flow-col py-2 gap-2 text-center">
-            <div class="shadow rounded-box text-white text-shadow-xs flex flex-col pt-8 pb-4 px-7 text-2xl" :class="round.mode === 'study' ? 'bg-red-400' : 'bg-green-500'">
+            <div class="shadow rounded-box text-white text-shadow-xs flex flex-col pt-8 pb-4 px-7 text-2xl" :class="round.mode === 'study' ? 'bg-red-600' : 'bg-green-600'">
                 <span class="countdown font-mono text-9xl font-black">
                     <span :style="{ '--value': minutes }" aria-live="polite" :aria-label="String(minutes)"></span>
                 </span>
                 min
             </div>
-            <div class="shadow rounded-box text-white text-shadow-xs flex flex-col pt-8 pb-4 px-7 text-2xl" :class="round.mode === 'study' ? 'bg-red-400' : 'bg-green-500'">
+            <div class="shadow rounded-box text-white text-shadow-xs flex flex-col pt-8 pb-4 px-7 text-2xl" :class="round.mode === 'study' ? 'bg-red-600' : 'bg-green-600'">
                 <span class="countdown font-mono text-9xl font-black">
                     <span :style="{ '--value': seconds }" aria-live="polite" :aria-label="String(seconds)"></span>
                 </span>
--- src/components/timeline/TimeLine.vue
+++ src/components/timeline/TimeLine.vue
@@ -29,22 +29,23 @@ const goForwardInTime = () => {
 </script>
 
 <template>
-  <div class="h-5 z-10">
-    <button @click="goForwardInTime()" v-if="records.length > sliceEnd - sliceStart && sliceStart !== 0" class="btn w-full bg-gray-50 border-gray-100 h-7 text-gray-300">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1024 1024"><path fill="currentColor" d="m488.832 344.32l-339.84 356.672a32 32 0 0 0 0 44.16l.384.384a29.44 29.44 0 0 0 42.688 0l320-335.872l319.872 335.872a29.44 29.44 0 0 0 42.688 0l.384-.384a32 32 0 0 0 0-44.16L535.168 344.32a32 32 0 0 0-46.336 0"/></svg>
-    </button>
-  </div>
-    
-
     <div class="timeline-overlay"></div>
     <ul class="timeline timeline-vertical mt-3">
         <TimeLineBubble v-for="record in records.slice(sliceStart, sliceEnd)" :key="record.date" :record="record" :records-length="records.length" />
     </ul>
 
-    <div class="h-5 z-10">
-      <button @click="goBackInTime()" v-if="records.length > sliceEnd - sliceStart" class="btn w-full bg-gray-50 border-gray-100 h-7 text-gray-300">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1024 1024"><path fill="currentColor" d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z"/></svg>
-      </button>
+    <div class="h-5 z-10 flex">
+      <div class="w-50">
+        <button @click="goBackInTime()" v-if="records.length > sliceEnd - sliceStart && sliceEnd <= records.length" class="btn w-50 bg-gray-50 border-gray-100 h-7 text-gray-300">
+           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1024 1024"><path fill="currentColor" d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z"/></svg>
+        </button>
+      </div>
+      
+      <div class="w-50">
+        <button @click="goForwardInTime()" v-if="records.length > sliceEnd - sliceStart && sliceStart !== 0" class="btn w-50 bg-gray-50 border-gray-100 h-7 text-gray-300">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1024 1024"><path fill="currentColor" d="m488.832 344.32l-339.84 356.672a32 32 0 0 0 0 44.16l.384.384a29.44 29.44 0 0 0 42.688 0l320-335.872l319.872 335.872a29.44 29.44 0 0 0 42.688 0l.384-.384a32 32 0 0 0 0-44.16L535.168 344.32a32 32 0 0 0-46.336 0"/></svg>
+        </button>
+      </div>
     </div>
 </template>
 
